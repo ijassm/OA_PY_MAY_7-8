734ade3e5c24baa93ab2ae0563e06c20f0f2d53e
“ijassm” <ijass.ocean@gmail.com>
2023-06-20 20:31:08 +0530

    print task completed

---
+++ Pravin/task1.py
@@ -0,0 +1,23 @@
+# user = input("enter your name: ")
+user = "SUMI"
+
+l = len(user)
+
+# for i in range(1, 6):
+#     if (i == 1 or i == 5):
+#         print(" -----------     " * l)
+#     elif (i == 2 or i == 4):
+#         print("|           |    " * l)
+#     elif (i == 3):
+#         for j in range(l):
+#             print(f"|     {user[j]}     |    ", end="")
+#         print()
+
+# h = '-'*11
+# s5 = ' '*5
+# s11 = ' '*11
+
+
+# for i in range(l):
+#     print(
+#         f" {h}\n|{s11}|\n|{s5}{user[i]}{s5}|\n|{s11}|\n {h}")

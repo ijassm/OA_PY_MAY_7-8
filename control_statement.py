d96a0d0cad332863c6315a3ea2f2b74641497c58
“ijassm” <ijass.ocean@gmail.com>
2023-06-26 20:31:16 +0530

    control statement

---
+++ control_statement.py
@@ -0,0 +1,20 @@
+# for i in range(11):
+#     if (i % 2 == 0):
+#         print(i, "even")
+#         continue
+#     else:
+#         print(i, "odd")
+
+#     print("n number lines of code")
+
+# print("---")
+
+# for i in range(11):
+#     if (i % 2 != 0):
+#         print(i, "odd")
+
+
+# for i in range(5):
+#     print(i-1)
+#     if (i % 2 == 0):
+#         break

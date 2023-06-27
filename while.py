7d9dec807940f6d55dbff94d650e30fdc3f2ae76
“ijassm” <ijass.ocean@gmail.com>
2023-06-27 20:01:28 +0530

    while loop

---
+++ while.py
@@ -0,0 +1,22 @@
+i = 0
+
+while (i < 5):
+    i += 1
+    print(i)
+
+# 0 < 5 = True
+# i = 1
+
+# 1 < 5 = True
+# i = 2
+
+# 2 < 5 = True
+# i = 3
+
+# 3 < 5 = True
+# i = 4
+
+# 4 < 5 = True
+# i = 5
+
+# 4 < 5 = False

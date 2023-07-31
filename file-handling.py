ced0c28dc6a06e15c676652cdf5ba2a38249ecc6
“ijassm” <ijass.ocean@gmail.com>
2023-07-31 20:06:48 +0530

    file-handling - 31/07/2023

---
+++ file-handling.py
@@ -0,0 +1,34 @@
+# r = open("demo.txt", "rt")
+# w = open("demo.txt", "wt")
+# a = open("demo.txt", "at")
+# import os
+
+
+# print(r.read())
+# print(r.read(3))
+
+# for i in r:
+#     print(i)
+
+# print(r.readline())
+# print(r.readline())
+
+
+# w.write("Am studying python")
+# w.close()
+
+# for i in range(1, 100):
+#     a = open(f"demo{i}.txt", "a")
+#     a.write(f'{i}')
+
+# w.close()
+
+
+# a.write("Am studying python")
+# a.close()
+
+# print(r.read())
+# r.close()
+
+# for i in range(1, 2):
+#     os.remove(f"demo{i}.txt")
